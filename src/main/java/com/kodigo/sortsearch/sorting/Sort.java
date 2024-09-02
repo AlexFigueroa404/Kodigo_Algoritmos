dac3887f31e4bbb1db83bb8fd3921596dda79a2e
Alex Figueroa <alex.figue503@gmail.com>
2024-09-02 15:48:15 -0600

    feat: add insertionSort implementation for int arrays

--- src/main/java/com/kodigo/sortsearch/sorting/Sort.java
+++ src/main/java/com/kodigo/sortsearch/sorting/Sort.java
@@ -48,7 +48,21 @@ public class Sort {
 
     }
 
-    public void insertionSort() {
+    public void insertionSort(int[] arr) {
+
+        int temp, j;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            temp = arr[i];
+            j = i;
+
+            while (j > 0 && arr[j - 1] > temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
 
     }
 
