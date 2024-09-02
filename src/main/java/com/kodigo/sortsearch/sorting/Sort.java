8c14d91fd7053d18b2d97c6602984f1f9616580e
Alex Figueroa <alex.figue503@gmail.com>
2024-09-02 15:42:01 -0600

    feat: add selectionSort implementation for int arrays

--- src/main/java/com/kodigo/sortsearch/sorting/Sort.java
+++ src/main/java/com/kodigo/sortsearch/sorting/Sort.java
@@ -9,9 +9,9 @@ public class Sort {
         for (int i = 0; i < arr.length - 1; i++) { // hasta n - 1 iteraciones porque el último elemento ya estará ordenado
             for (int j = 0; j < arr.length - i - 1; j++) {
                 /*
-                * Se resta i porque en cada iteración el elemento más grande se coloca al final por lo que no es necesario
-                * comparar los elementos que ya están ordenados
-                * */
+                 * Se resta i porque en cada iteración el elemento más grande se coloca al final por lo que no es necesario
+                 * comparar los elementos que ya están ordenados
+                 * */
                 if (arr[j] > arr[j + 1]) {
                     swap(arr, j, j + 1);
                     swapped = true;
@@ -26,6 +26,24 @@ public class Sort {
     }
 
     public void selectionSort(int[] arr) {
+
+        int minIndex;
+        for (int i = 0; i < arr.length - 1; i++) {
+            minIndex = i; // se asume que el primer elemento es el menor y se actualiza si se encuentra un valor menor
+            for (int j = i + 1; j < arr.length; j++) {
+                /*
+                 * j = i + 1 porque se entiende que el valor anterior ya esta "ordenado"
+                 *
+                 * */
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) { // si el valor mínimo no es el mismo que el valor actual, se intercambian
+                swap(arr, i, minIndex);
+            }
+        }
 
 
     }
